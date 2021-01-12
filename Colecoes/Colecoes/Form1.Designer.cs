21fc61ba3f6b7b9b435339f64629584dbfd0603d
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2021-01-11 22:49:53 -0300

    11-01

---
+++ Colecoes/Colecoes/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Colecoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnList = new System.Windows.Forms.Button();
+            this.listaBox = new System.Windows.Forms.ListBox();
+            this.btnHashSet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.Location = new System.Drawing.Point(534, 12);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(133, 41);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // listaBox
+            // 
+            this.listaBox.FormattingEnabled = true;
+            this.listaBox.Location = new System.Drawing.Point(24, 10);
+            this.listaBox.Name = "listaBox";
+            this.listaBox.Size = new System.Drawing.Size(468, 407);
+            this.listaBox.TabIndex = 2;
+            // 
+            // btnHashSet
+            // 
+            this.btnHashSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHashSet.Location = new System.Drawing.Point(534, 59);
+            this.btnHashSet.Name = "btnHashSet";
+            this.btnHashSet.Size = new System.Drawing.Size(133, 41);
+            this.btnHashSet.TabIndex = 3;
+            this.btnHashSet.Text = "HashSet";
+            this.btnHashSet.UseVisualStyleBackColor = true;
+            this.btnHashSet.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 450);
+            this.Controls.Add(this.btnHashSet);
+            this.Controls.Add(this.listaBox);
+            this.Controls.Add(this.btnList);
+            this.Name = "Form1";
+            this.Text = "Listas Genericas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.ListBox listaBox;
+        private System.Windows.Forms.Button btnHashSet;
+    }
+}
+
---
+++ Colecoes/Colecoes/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            listaBox.Items.Clear();
+            string[] nomes = new string[3];
+            nomes[0] = "Filipe";
+            nomes[1] = "arthur";
+            nomes[2] = "Danny";
+
+            List<string> nomes2 = new List<string>();
+            nomes2.Add("Lucas");
+            nomes2.Add("Beatriz");
+            nomes2.AddRange(nomes);
+            //adiciona um array na lista
+            //if (nomes2.Remove("Lucas"))//remove
+            //{
+            //    MessageBox.Show("Lucas removido");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("não foi possivel remover");
+            //}
+            //if (nomes2.Contains("Filipe"))
+            //{
+            //    MessageBox.Show("contém!");
+            //}
+            //else
+            //{
+            //  MessageBox.Show("não contém!");
+            //}
+            //listaBox.Items.Add(nomes2[1]);
+            //listaBox.Items.Add(nomes2[0]);
+            //listaBox.Items.AddRange(nomes);
+            // MessageBox.Show("Numero de elementos: " + nomes2.Count);//conta quantos elementos tem
+            //nomes2.Sort();
+            // MessageBox.Show($"Filipe esta no indice: {nomes2.IndexOf("Filipe")}");  
+            // nomes2.Insert(2, "Joao");
+            nomes2.RemoveAt(1);
+
+            foreach (string nome in nomes2)
+            {
+                listaBox.Items.Add(nome);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listaBox.Items.Clear();
+            HashSet<string> veiculos = new HashSet<string>()//hashSet nao aceita adicionar um item que ja tem na lista
+            { 
+                "Carro","Moto","Avião","Helicoptero", "Barco"
+            };
+            MessageBox.Show(veiculos.ElementAt(2));
+            MessageBox.Show(veiculos.First());
+            MessageBox.Show(veiculos.Last());
+            //if (veiculos.Add("Cavalo"))
+            //{
+            //    MessageBox.Show("Item adicionado");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Item não adicionado");
+            //}
+
+            foreach (string item in veiculos)
+            {
+                listaBox.Items.Add(item);
+            }
+
+        }
+    }
+}
