e626d20b60c480e1720e61e205f639101be915eb
Filipe Augusto <f04a07s19c93@gmail.com>
2021-02-12 16:43:23 -0300

    added projects
    about classe File and Directory

---
+++ ArquivoPastas/Arquivo/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arquivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //  File
+
+            string pasta = @"c:\curso\";
+            string arquivo = "teste.txt";
+            //bool res = File.Exists(pasta + arquivo);
+            //if (res)
+            //{
+            //    MessageBox.Show("Existe!");
+            //}
+            //DELETAR
+            //File.Delete(pasta + arquivo);
+
+            //CRIAR-para nao deletar e criar outro
+            //if (!File.Exists(pasta + arquivo))//se nao existir, 
+            //{
+            //    File.Create(pasta + arquivo).Close();//criar e fechar
+            //    label1.Text = "Arquivo criado!";
+            //}
+            //else
+            //{
+            //    label1.Text = "Arquivo ja existe!";
+            //}
+            //COPIAR 
+            //if (File.Exists(pasta+arquivo))
+            //{
+            //    File.Copy(pasta + arquivo, pasta + "copia.txt", true); //true permite sobrescrever
+            //    label1.Text = "Copiado";
+            //}
+            //else
+            //{
+            //    label1.Text = "Arquivo não existe";
+            //}
+
+            //MOVE
+            //if(File.Exists(pasta + arquivo))
+            //{
+            //    File.Move(pasta + arquivo, pasta + "destino\\" + arquivo);
+            //    label1.Text = "Arquivo movido!";
+            //}
+            //else if(File.Exists(pasta + "destino\\" + arquivo) && !File.Exists(pasta + arquivo))
+            //{
+            //    label1.Text = "Arquivo já foi movido!";
+            //    label1.ForeColor = Color.Red;
+            //}
+            //else
+            //{
+            //    label1.Text = "Arquivo não existe";
+            //}
+
+            //File.WriteAllText(pasta + arquivo, "teste de escrita de arquivo novo", Encoding.Default);//escrever
+            //label1.Text = File.ReadAllText(pasta + arquivo, Encoding.Default); //ler
+
+            label1.Text = "Criado em: "+ File.GetCreationTime(pasta + arquivo).ToString();
+            label1.Text += "ultima data de acesso : " + File.GetLastAccessTime(pasta + arquivo).ToString();
+           File.SetCreationTime(pasta + arquivo,DateTime.Now);
+            label1.Text += "mudando a data de criado para: " + File.GetCreationTime(pasta + arquivo).ToString();
+        }
+    }
+}
---
+++ ArquivoPastas/Diretorio/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Diretorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            string path = @"c:\curso\";
+            bool res = Directory.Exists(path + "");
+            // lista.Items.Add(res);
+            //if(!res) //se nao existe ele cria
+            //{
+            //Directory.CreateDirectory(path + "logan");
+            //}
+
+            //if (res) //se existe ele deleta
+            //{
+            //    Directory.Delete(path + "logan");
+            //}
+
+            //if (true)// se existe ele move
+            //{
+            //    Directory.Move(path + "logan", path + @"destino\logan");
+            //}
+
+            //string[] dirs = Directory.GetDirectories(path);
+
+            //foreach(string d in dirs) // pega os diretorios
+            //{
+            //    lista.Items.Add(d);
+            //}
+
+            //string[] files = Directory.GetDirectories(path);
+            //foreach (string f in files) // pega os as pastas
+            //{
+            //    lista.Items.Add(f);
+            //}
+
+            //string root =Directory.GetDirectoryRoot(path);
+            //lista.Items.Add(root);
+
+            //string[] drives = Directory.GetLogicalDrives(); //todas as pastas raizes
+            //lista.Items.AddRange(drives);
+
+           //DirectoryInfo info =  Directory.GetParent(path);
+           // lista.Items.Add(info.FullName);
+          string dir =  Directory.GetCurrentDirectory();
+            lista.Items.Add(dir);
+        }
+    }
+}
