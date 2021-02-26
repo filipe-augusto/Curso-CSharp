9a2ba03fb1957d2afe06c06bf93f37abbdaef65c
Filipe Augusto <f04a07s19c93@gmail.com>
2021-02-26 16:33:48 -0300

    added project about sockets

---
+++ ChatCliente/ChatCliente/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace ChatCliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.numPortaHost = new System.Windows.Forms.NumericUpDown();
+            this.txtServidorIp = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPortaHost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(504, 68);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(120, 31);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // numPortaHost
+            // 
+            this.numPortaHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numPortaHost.Location = new System.Drawing.Point(504, 26);
+            this.numPortaHost.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numPortaHost.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numPortaHost.Name = "numPortaHost";
+            this.numPortaHost.Size = new System.Drawing.Size(120, 31);
+            this.numPortaHost.TabIndex = 2;
+            this.numPortaHost.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // txtServidorIp
+            // 
+            this.txtServidorIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServidorIp.Location = new System.Drawing.Point(38, 26);
+            this.txtServidorIp.Name = "txtServidorIp";
+            this.txtServidorIp.Size = new System.Drawing.Size(446, 31);
+            this.txtServidorIp.TabIndex = 3;
+            this.txtServidorIp.Text = "127.0.0.1";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(38, 68);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(446, 31);
+            this.txtUsuario.TabIndex = 4;
+            this.txtUsuario.Text = "Visitante";
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(38, 527);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(460, 31);
+            this.txtMensagem.TabIndex = 5;
+            this.txtMensagem.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMensagem_KeyPress);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(504, 527);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(120, 31);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(38, 561);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(586, 61);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "não conectado";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(38, 117);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(586, 381);
+            this.txtLog.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 631);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtServidorIp);
+            this.Controls.Add(this.numPortaHost);
+            this.Controls.Add(this.btnConectar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Chat Server";
+            ((System.ComponentModel.ISupportInitialize)(this.numPortaHost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.NumericUpDown numPortaHost;
+        private System.Windows.Forms.TextBox txtServidorIp;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ ChatCliente/ChatCliente/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatCliente
+{
+    public partial class Form1 : Form
+    {
+
+        //trata o nome do usuario
+        private string NomeUsuario;
+        private StreamWriter stwEnviador;
+        private StreamReader strReceptor;
+        private TcpClient tcpServidor;
+        //necessario para atualizar o formulario com mensagens da outra thread
+        private delegate void AtualizaLogCallBack(string strMensagem);
+        //necessario para definir o formulario para o estado disconnected de outra thread
+        private delegate void FechaConexaoCallBack(string strMotivo);
+        private Thread mensagemThread;
+        private IPAddress enderecoIP;
+        private int portaHost;
+        private bool Conectado;
+
+
+        public Form1()
+        {
+            //na saida da aplicação  : desconectar
+            Application.ApplicationExit += new EventHandler(OnAplicationExit);
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EnviaMensagem();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //se nao esta conectando aguarda a conexão
+            if (!Conectado)
+            {
+                InicializaConexao();
+            }
+            else
+            {
+                FechaConexao("Desconectado a pedido do usuario.");
+            }
+        }
+
+
+        private void txtMensagem_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //se pressionou a tecla enter
+            if (e.KeyChar == (char)13)
+            {
+                EnviaMensagem();
+            }
+        }
+        private void InicializaConexao()
+        {
+            try
+            {
+                //trata o endereço ip informado em um objeto ipadress
+                enderecoIP = IPAddress.Parse(txtServidorIp.Text);
+                //trata o numero da porta do host 
+                portaHost = (int)numPortaHost.Value;
+                //inicia uma nova conexão TCP com o servidor chat
+                tcpServidor = new TcpClient();
+                tcpServidor.Connect(enderecoIP, portaHost);
+                //ajuda a verificar se estamos conectados ou não 
+                Conectado = true;
+
+                //preparar o formulario
+                NomeUsuario = txtUsuario.Text;
+                //desabilitar e habilitar os campos apropriados
+                txtServidorIp.Enabled = false;
+                numPortaHost.Enabled = false;
+                txtUsuario.Enabled = false;
+                txtMensagem.Enabled = true;
+                btnConectar.ForeColor = Color.Red;
+                btnConectar.Text = "Desconectar";
+
+                //envia o nome do usuario ao servidor
+                stwEnviador = new StreamWriter(tcpServidor.GetStream());
+                stwEnviador.WriteLine(txtUsuario.Text);
+                stwEnviador.Flush();
+                //inicia  a thread para receber mensagens e nova comunicação
+                mensagemThread = new Thread(new ThreadStart(RecebeMensagens));
+                mensagemThread.IsBackground = true;
+                mensagemThread.Start();
+                labelStatus.Invoke(new Action(() =>
+                {
+                    labelStatus.ForeColor = Color.Green;
+                    labelStatus.Text = $"Conectado ao servidor de chat{enderecoIP}:{portaHost}";
+                }));
+
+
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Invoke(new Action(() =>
+                {
+                    labelStatus.ForeColor = Color.Red;
+                    labelStatus.Text = "Erro na conexão com o servidor :\n" + ex.Message;
+                }));
+            }
+        }
+        private void RecebeMensagens()
+        {
+            //recebe a resposta do servidor
+            strReceptor = new StreamReader(tcpServidor.GetStream());
+            string ConResposta = strReceptor.ReadLine();
+            //se o primeiro formulario para informar que esta conectado
+            if (ConResposta[0] == '1')
+            {
+                //atualiza o formulario para informar que esta conectado
+                this.Invoke(new AtualizaLogCallBack(this.AtualizaLog), new object[]
+                {"conectado com sucesso!"
+                });
+            }
+            else
+            {
+                //se o primeiro caractere não for 1 a conexão falhou
+                string Motivo = "não conectado: ";
+                //extrai o motivo da mensagem resposta, o moitvo começa no 3 caractere
+                Motivo += ConResposta.Substring(2, ConResposta.Length - 2);
+                //atualiza o formulario com o motivo da falha de conexão
+                this.Invoke(new FechaConexaoCallBack(this.FechaConexao), new object[] { Motivo });
+                // sai do metodo
+                return;
+            }
+
+            //enquanto estiver conectado le as linhas que estaõ chegando do servidor
+            while (Conectado)
+            {
+                this.Invoke(new FechaConexaoCallBack(this.AtualizaLog), new object[] { strReceptor.ReadLine() });
+            }
+
+        }
+        private void AtualizaLog(string strMensagem)
+        {
+            //anexa texto ao final de cada linha
+            txtLog.AppendText(strMensagem + "\r\n");
+
+        }
+        private void EnviaMensagem()
+        {
+            //envia a mensagem para o servidor
+            if (txtMensagem.Lines.Length >=1)
+            {
+                stwEnviador.WriteLine(txtMensagem.Text);
+                stwEnviador.Flush();
+                txtMensagem.Lines = null;
+            }
+            txtMensagem.Text = "";
+        }
+        private void FechaConexao(string Motivo)
+        {
+            //fecha a conexão com o servidor
+
+            //mostra o motivo proque a conexão encerrou
+            txtLog.AppendText(Motivo + "\r\n");
+            //habilita e desabilita os contrles apropriados no formulario
+            txtServidorIp.Enabled = true;
+            numPortaHost.Enabled = true;
+            txtUsuario.Enabled = true;
+            txtMensagem.Enabled = false;
+            btnEnviar.Enabled = false;
+            btnConectar.ForeColor = Color.Green;
+            btnConectar.Text = "conectado";
+
+            //fecha objetos
+            Conectado = false;
+            stwEnviador.Close();
+            strReceptor.Close();
+            tcpServidor.Close();
+            labelStatus.Invoke(new Action(() =>
+            {
+                labelStatus.ForeColor = Color.Green;
+                labelStatus.Text = $"Desconectadoo do servidor de chat{enderecoIP} {portaHost} ";
+            }));
+
+        }
+        //o tratador  de evento para a saida da aplicação
+        public void OnAplicationExit(object Sender, EventArgs e)
+        {
+            if (Conectado)
+            {
+                //fecha as conexoes, streams etc
+                Conectado = false;
+                stwEnviador.Close();
+                strReceptor.Close();
+                tcpServidor.Close();
+                labelStatus.Invoke(new Action(() =>
+                {
+                    labelStatus.ForeColor = Color.Green;
+                    labelStatus.Text = $"Desconectadoo do servidor de chat{enderecoIP} {portaHost} ";
+                }));
+            }
+        }
+    }
+}
