99302fdc7e0216926802ee3d5847e63aa355028a
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2021-03-01 21:47:24 -0300

    projeto teste

---
+++ BancoDeDados_Data_Grid_File/BancoDeDados_Data_Grid_File/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace BancoDeDados_Data_Grid_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtImagem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dataGridViewLista = new System.Windows.Forms.DataGridView();
+            this.NOME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DATAINCLUSAO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DESCRICAO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IMAGEM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DOWNLOAD = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.EXCLUSAO = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txtDescricao);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtImagem);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Location = new System.Drawing.Point(24, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(743, 168);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescricao.Location = new System.Drawing.Point(101, 78);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(483, 83);
+            this.txtDescricao.TabIndex = 7;
+            this.txtDescricao.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Descrição";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(615, 79);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(102, 80);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Imagem";
+            // 
+            // txtImagem
+            // 
+            this.txtImagem.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtImagem.Enabled = false;
+            this.txtImagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImagem.Location = new System.Drawing.Point(101, 46);
+            this.txtImagem.Name = "txtImagem";
+            this.txtImagem.Size = new System.Drawing.Size(483, 24);
+            this.txtImagem.TabIndex = 3;
+            this.txtImagem.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(101, 16);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(483, 24);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.Text = "Insira o nome";
+            // 
+            // dataGridViewLista
+            // 
+            this.dataGridViewLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewLista.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewLista.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridViewLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NOME,
+            this.DATAINCLUSAO,
+            this.DESCRICAO,
+            this.IMAGEM,
+            this.DOWNLOAD,
+            this.EXCLUSAO});
+            this.dataGridViewLista.Location = new System.Drawing.Point(24, 187);
+            this.dataGridViewLista.Name = "dataGridViewLista";
+            this.dataGridViewLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewLista.Size = new System.Drawing.Size(743, 206);
+            this.dataGridViewLista.TabIndex = 1;
+            this.dataGridViewLista.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLista_CellContentClick);
+            this.dataGridViewLista.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLista_CellDoubleClick);
+            // 
+            // NOME
+            // 
+            this.NOME.HeaderText = "NOME";
+            this.NOME.Name = "NOME";
+            // 
+            // DATAINCLUSAO
+            // 
+            this.DATAINCLUSAO.HeaderText = "DATA INCLUSÃO";
+            this.DATAINCLUSAO.Name = "DATAINCLUSAO";
+            // 
+            // DESCRICAO
+            // 
+            this.DESCRICAO.HeaderText = "DESCRIÇÃO";
+            this.DESCRICAO.Name = "DESCRICAO";
+            // 
+            // IMAGEM
+            // 
+            this.IMAGEM.HeaderText = "IMAGEM";
+            this.IMAGEM.Name = "IMAGEM";
+            // 
+            // DOWNLOAD
+            // 
+            this.DOWNLOAD.HeaderText = "DOWNLOAD";
+            this.DOWNLOAD.Name = "DOWNLOAD";
+            // 
+            // EXCLUSAO
+            // 
+            this.EXCLUSAO.HeaderText = "EXCLUIR";
+            this.EXCLUSAO.Name = "EXCLUSAO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 419);
+            this.Controls.Add(this.dataGridViewLista);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLista)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtImagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DataGridView dataGridViewLista;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DATAINCLUSAO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DESCRICAO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IMAGEM;
+        private System.Windows.Forms.DataGridViewButtonColumn DOWNLOAD;
+        private System.Windows.Forms.DataGridViewButtonColumn EXCLUSAO;
+    }
+}
+
---
+++ BancoDeDados_Data_Grid_File/BancoDeDados_Data_Grid_File/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancoDeDados_Data_Grid_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+        static string strConection = @"Data Source=FILIPE-HPRPE60;Initial Catalog=PrimeiroBanco;Integrated Security=True";
+        // SqlConnection conexao = new SqlConnection(strConection);
+
+        private void CarregarGrid()
+        {
+            try
+            {
+                using (var conexao = AbrirConexao())
+                {
+                    conexao.Open();
+                    string query = "select id, nome, datainclusao , descricao from imagens";
+                    DataTable dados = new DataTable();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, strConection);
+                    /////////////////
+                    adaptador.Fill(dados);
+                    foreach (DataRow linha in dados.Rows)
+                    {
+                        dataGridViewLista.Rows.Add(linha.ItemArray);//insere a linha na datagriw
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("erro");
+            }
+
+        }
+        private IDbConnection AbrirConexao()
+        {
+            return new SqlConnection(strConection);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var arquivo = EscolherArquivo();
+                if (!string.IsNullOrWhiteSpace(arquivo))
+                {
+                    SalvarAquivo(arquivo);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string EscolherArquivo()
+        {
+            var retorno = string.Empty;
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    retorno = dialog.FileName;
+                    txtImagem.Text = retorno;
+
+                }
+
+            }
+            return retorno;
+        }
+        private void SalvarAquivo(string arquivo)
+        {
+            using (var conexao = AbrirConexao())
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "INSERT INTO imagens (nome,datainclusao, descricao, foto, extencao) values" +
+                        $"(@Nome,@DataInclusao, @Descricao,@Foto,@Extecao)";
+                    ConfigurarParametros(comando, arquivo);
+                    comando.ExecuteNonQuery();
+                    CarregarGrid();
+                }
+            }
+        }
+
+        private void ConfigurarParametros(IDbCommand comm, string arquivo)
+        {
+            comm.Parameters.Add(new SqlParameter("Nome", txtNome.Text));
+            comm.Parameters.Add(new SqlParameter("DataInclusao", DateTime.Now));
+            comm.Parameters.Add(new SqlParameter("Descricao", txtDescricao.Text));
+            comm.Parameters.Add(new SqlParameter("Foto", File.ReadAllBytes(arquivo)));
+            comm.Parameters.Add(new SqlParameter("Extecao", Path.GetExtension(arquivo)));
+        }
+
+        private void dataGridViewLista_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //MessageBox.Show("teste");
+        }
+
+        private void dataGridViewLista_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("teste");
+        }
+    }
+}
