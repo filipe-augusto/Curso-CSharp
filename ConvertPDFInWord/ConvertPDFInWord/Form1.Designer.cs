892b811096ff4d44730671d6530e7fa8a7993032
Filipe Augusto <f04a07s19c93@gmail.com>
2021-03-15 16:46:12 -0300

    converter pdf to word

---
+++ ConvertPDFInWord/ConvertPDFInWord/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace ConvertPDFInWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCaminho = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblConversao = new System.Windows.Forms.LinkLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(762, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Converter para Word";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCaminho
+            // 
+            this.lblCaminho.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaminho.Location = new System.Drawing.Point(4, 22);
+            this.lblCaminho.Name = "lblCaminho";
+            this.lblCaminho.Size = new System.Drawing.Size(644, 68);
+            this.lblCaminho.TabIndex = 1;
+            this.lblCaminho.Text = "...";
+            this.lblCaminho.Click += new System.EventHandler(this.lblCaminho_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Image = global::ConvertPDFInWord.Properties.Resources.exit1;
+            this.button2.Location = new System.Drawing.Point(702, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 92);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblConversao
+            // 
+            this.lblConversao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConversao.Location = new System.Drawing.Point(3, 22);
+            this.lblConversao.Name = "lblConversao";
+            this.lblConversao.Size = new System.Drawing.Size(754, 94);
+            this.lblConversao.TabIndex = 4;
+            this.lblConversao.TabStop = true;
+            this.lblConversao.Text = "...";
+            this.lblConversao.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblConversao_LinkClicked);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblCaminho);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Location = new System.Drawing.Point(26, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 92);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Arquivo Convertido";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.lblConversao);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel2.Location = new System.Drawing.Point(24, 180);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(764, 120);
+            this.panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 312);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCaminho;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel lblConversao;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ ConvertPDFInWord/ConvertPDFInWord/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SautinSoft;
+
+namespace ConvertPDFInWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string nomeArquivo;
+        string caminho;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                lblConversao.Text = "";
+                lblCaminho.Text = "";
+            try
+            {
+
+                SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+                 caminho = @escolherArquivo();
+                f.OpenPdf(@caminho);
+                //string caminho = (Environment.GetFolderPath caminho);
+
+                if (f.PageCount > 0)
+                {
+                    string destino = @Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).ToString() + @"\Conversao"
+                    + nomeArquivo.Replace(".","") + ".docx";
+                    f.WordOptions.Format = SautinSoft.PdfFocus.CWordOptions.eWordDocument.Docx;
+                    f.ToWord(destino);
+
+                    lblConversao.ForeColor = Color.Green;
+                    lblConversao.Text = destino;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                lblConversao.ForeColor = Color.Red;
+                lblConversao.Text = "Erro:  "+ ex.Message;
+               
+            }
+
+        }
+
+        public string escolherArquivo()
+        {
+            var retorno = string.Empty;
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    retorno = dialog.FileName;
+                    lblCaminho.Text = retorno;
+                    FileInfo info = new FileInfo(retorno);
+                    nomeArquivo = info.Name;
+                }
+            }
+            return retorno;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblConversao_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                var diretorio = Path.GetDirectoryName(lblConversao.Text);
+                var nomearquivo = Path.GetFileName(lblConversao.Text);
+                var arquivo = new FileInfo(lblConversao.Text);
+                lblConversao.Text = diretorio +@"\"+ nomearquivo;
+                Process.Start(lblConversao.Text);
+
+            }
+            catch (Exception ex)
+            {
+                lblConversao.ForeColor = Color.Red;
+                lblConversao.Text = "Erro:  " + ex.Message + "\n" ;
+            }
+        }
+
+        private void lblCaminho_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
