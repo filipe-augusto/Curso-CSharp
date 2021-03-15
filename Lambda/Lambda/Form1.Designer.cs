6b74c21397aba9b510d06ca2ee41a73b85d1d4ae
Filipe Augusto <f04a07s19c93@gmail.com>
2021-03-15 08:33:46 -0300

    added project about lambda

---
+++ Lambda/Lambda/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Lambda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(590, 347);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(165, 58);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(729, 231);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lambda/Lambda/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lambda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //expressão lambda que tem uma expressão como corpo:
+            //(input - parameters) => expression
+
+            //instrução lambda que tem um bloco de instrução como corpo
+            //(input - parameters) => { < sequence - of  - statements >}
+
+            //Func<int, int> square =  quadrado; //delegate
+            // Func<int, int> square = x => x * x;
+
+            // quadrado(2);
+            // ///label1.Text = "O resultado é " + square(5);
+            // Expression<Func<int, int, int>> ex = (x , y) => x + y;
+            //  label1.Text = "O resultado é " + ex.ToString();
+            // //int[] numbers = { 2, 3, 4, 5 };
+            // //var squaredNumbers = numbers.Select(x => x * x);
+            // //label1.Text = string.Join(" ", squaredNumbers);
+
+
+            // Action<string> greet = name =>
+            //{
+            //    string greeting = $"hello {name}!";
+            //    label1.Text = greeting;
+            //};
+            // greet("Gabriel");
+
+            // Action line = () => Console.WriteLine();
+            // Console.WriteLine("Gabriel " + line + " Arttigas");
+            // line();
+
+            Func<int, int, bool> testeForEquality = (x, y) => x == y;
+            label1.Text = testeForEquality(10, 15).ToString();
+
+
+        }
+        int quadrado(int x)
+        {
+            return x * x;
+        }
+
+    }
+}
