dd639f19631f9813c72ba9c9468c3c8549562824
Filipe Augusto <f04a07s19c93@gmail.com>
2021-03-22 20:01:35 -0300

    graficos

---
+++ Graficos/Graficos/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Graficos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValorX = new System.Windows.Forms.TextBox();
+            this.txtValorY = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnLimparGraficos = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipoGrafico = new System.Windows.Forms.ComboBox();
+            this.palleteCor = new System.Windows.Forms.ComboBox();
+            this.grafico3D = new System.Windows.Forms.CheckBox();
+            this.grafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Graficos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(163, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor Y";
+            // 
+            // txtValorX
+            // 
+            this.txtValorX.Location = new System.Drawing.Point(13, 101);
+            this.txtValorX.Name = "txtValorX";
+            this.txtValorX.Size = new System.Drawing.Size(121, 20);
+            this.txtValorX.TabIndex = 3;
+            this.txtValorX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValorX_KeyPress);
+            // 
+            // txtValorY
+            // 
+            this.txtValorY.Location = new System.Drawing.Point(155, 101);
+            this.txtValorY.Name = "txtValorY";
+            this.txtValorY.Size = new System.Drawing.Size(124, 20);
+            this.txtValorY.TabIndex = 4;
+            this.txtValorY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValorY_KeyPress);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(13, 127);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(269, 39);
+            this.btnInserir.TabIndex = 5;
+            this.btnInserir.Text = "Inserir Vetores no Grafico";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnLimparGraficos
+            // 
+            this.btnLimparGraficos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimparGraficos.Location = new System.Drawing.Point(12, 447);
+            this.btnLimparGraficos.Name = "btnLimparGraficos";
+            this.btnLimparGraficos.Size = new System.Drawing.Size(270, 39);
+            this.btnLimparGraficos.TabIndex = 6;
+            this.btnLimparGraficos.Text = "Limpar Graficos";
+            this.btnLimparGraficos.UseVisualStyleBackColor = true;
+            this.btnLimparGraficos.Click += new System.EventHandler(this.btnLimparGraficos_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(739, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 45);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Inserir valores aleatorios";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.x,
+            this.y});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(269, 268);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // x
+            // 
+            this.x.HeaderText = "Eixo X";
+            this.x.Name = "x";
+            this.x.ReadOnly = true;
+            // 
+            // y
+            // 
+            this.y.HeaderText = "Eixo Y";
+            this.y.Name = "y";
+            this.y.ReadOnly = true;
+            // 
+            // tipoGrafico
+            // 
+            this.tipoGrafico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipoGrafico.FormattingEnabled = true;
+            this.tipoGrafico.Location = new System.Drawing.Point(13, 500);
+            this.tipoGrafico.Name = "tipoGrafico";
+            this.tipoGrafico.Size = new System.Drawing.Size(266, 28);
+            this.tipoGrafico.TabIndex = 9;
+            this.tipoGrafico.SelectedIndexChanged += new System.EventHandler(this.tipoGrafico_SelectedIndexChanged);
+            // 
+            // palleteCor
+            // 
+            this.palleteCor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.palleteCor.FormattingEnabled = true;
+            this.palleteCor.Location = new System.Drawing.Point(303, 500);
+            this.palleteCor.Name = "palleteCor";
+            this.palleteCor.Size = new System.Drawing.Size(288, 28);
+            this.palleteCor.TabIndex = 10;
+            this.palleteCor.SelectedIndexChanged += new System.EventHandler(this.palleteCor_SelectedIndexChanged);
+            // 
+            // grafico3D
+            // 
+            this.grafico3D.AutoSize = true;
+            this.grafico3D.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grafico3D.Location = new System.Drawing.Point(614, 504);
+            this.grafico3D.Name = "grafico3D";
+            this.grafico3D.Size = new System.Drawing.Size(105, 24);
+            this.grafico3D.TabIndex = 11;
+            this.grafico3D.Text = "Grafico 3D";
+            this.grafico3D.UseVisualStyleBackColor = true;
+            this.grafico3D.CheckedChanged += new System.EventHandler(this.grafico3D_CheckedChanged);
+            // 
+            // grafico
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.grafico.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.grafico.Legends.Add(legend1);
+            this.grafico.Location = new System.Drawing.Point(303, 21);
+            this.grafico.Name = "grafico";
+            this.grafico.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "X vs Y";
+            series1.YValuesPerPoint = 2;
+            this.grafico.Series.Add(series1);
+            this.grafico.Size = new System.Drawing.Size(562, 458);
+            this.grafico.TabIndex = 12;
+            this.grafico.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 567);
+            this.Controls.Add(this.grafico);
+            this.Controls.Add(this.grafico3D);
+            this.Controls.Add(this.palleteCor);
+            this.Controls.Add(this.tipoGrafico);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLimparGraficos);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtValorY);
+            this.Controls.Add(this.txtValorX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValorX;
+        private System.Windows.Forms.TextBox txtValorY;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnLimparGraficos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.ComboBox tipoGrafico;
+        private System.Windows.Forms.ComboBox palleteCor;
+        private System.Windows.Forms.CheckBox grafico3D;
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafico;
+    }
+}
+
---
+++ Graficos/Graficos/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graficos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            valores = new Dictionary<double, double>();
+        }
+
+        Dictionary<double, double> valores;
+
+
+        private void btnLimparGraficos_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+  
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtValorX.Text) || String.IsNullOrEmpty(txtValorX.Text)) 
+            {
+                MessageBox.Show("Os dois valores são obrigatorios");
+                return;
+            }
+            if (valores.ContainsKey(double.Parse(txtValorX.Text)))
+            {
+                valores[double.Parse(txtValorX.Text)] = double.Parse(txtValorY.Text);
+            }
+            else
+            {
+                valores.Add(double.Parse(txtValorX.Text), double.Parse(txtValorY.Text));
+            }
+
+            dataGridView1.Rows.Clear();
+            grafico.Series[0].Points.Clear();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tipoGrafico_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void palleteCor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grafico3D_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtValorX.Focus();
+            for (int i = 0; i < 35; i++)
+            {
+                tipoGrafico.Items.Add((SeriesChartType)i);
+            }
+
+            for (int i = 0; i < 13; i++)
+            {
+                palleteCor.Items.Add((ChartColorPalette)i);
+            }
+        }
+
+        private void txtValorX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8
+                && e.KeyChar != (char)13 && e.KeyChar != (char)44) || 
+                (txtValorX.Text.Contains((char)44) && e.KeyChar == (char)44))//se nao for digito e tbm sem backspace. e se ja tiver virgula 
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtValorY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8
+                && e.KeyChar != (char)13 && e.KeyChar != (char)44) ||
+                (txtValorY.Text.Contains((char)44) && e.KeyChar == (char)44))//se nao for digito e tbm sem backspace. e se ja tiver virgula 
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
